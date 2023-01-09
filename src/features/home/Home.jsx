ae5cdef7f0ce98360c3fcb1b930d86d5331adbc5
PPHelios <pphelios1@gmail.com>
2023-01-09 08:42:40 +0200

    fixed responsiveness for mobile

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -11,14 +11,17 @@ export const Home = () => {
       <main>
         <div className="home--images">
           <div className="home--images-img1">
-            <img src={require("../../assets/images/body1.jpg")}></img>
+            <img src={require("../../assets/images/body1.jpg")}
+            alt="artistic coffee package"/>
           </div>
           <div className="home--images-img2">
-            <img src={require("../../assets/images/body2.jpg")}></img>
+            <img src={require("../../assets/images/body2.jpg")}
+            alt="artistic coffee package"/>
             <Button text="Buy Now" />
           </div>
           <div className="home--images-img3">
-            <img src={require("../../assets/images/body3.jpg")}></img>
+            <img src={require("../../assets/images/body3.jpg")}
+            alt="artistic coffee package"/>
           </div>
         </div>
       </main>
@@ -28,16 +31,18 @@ export const Home = () => {
             A Hot Cup of <br />
             <span>Happiness</span>
           </h2>
-          <img src={require("../../assets/images/slogan1.jpg")} />
+          <img src={require("../../assets/images/slogan1.jpg")} 
+          alt="cup of coffee with heart shaped cream"/>
         </div>
         <div className="slogansSection--slogan">
-          <img src={require("../../assets/images/slogan2.jpg")} />
-          <div className="xxx">
+          <img src={require("../../assets/images/slogan2.jpg")} 
+          alt="heart of coffee beans" />
+          
             <h2 className="slogansSection--slogan-text text2">
               Better Beans, Better <br />
               <span className="span2">Coffee.</span>
             </h2>
-          </div>
+         
         </div>
       </section>
       <section className="artMeetsCoffee">
@@ -48,6 +53,7 @@ export const Home = () => {
           <img
             className="artMeetsCoffee--grid-img1"
             src={require("../../assets/images/art1.jpg")}
+            alt="artistic coffee package"
           />
           <div className="artMeetsCoffee--grid-header h2meets">
             <h2>meets</h2>
@@ -55,25 +61,23 @@ export const Home = () => {
           <img
             className="artMeetsCoffee--grid-img2"
             src={require("../../assets/images/art2.jpg")}
+            alt="artistic coffee package"
           />
           <div className="artMeetsCoffee--grid-header h2coffee">
             <h2>coffee</h2>
           </div>
           <img
             className="artMeetsCoffee--grid-img3"
-            src={require("../../assets/images/art3.jpg")}
+            src={require("../../assets/images/art3.jpg")} 
+            alt="artistic coffee package"
           />
         </div>
       </section>
-      <section className="ourproducts">
-        <div className="flex">
-          <div className="box red"></div>
-          <div className="box green"></div>
-          <div className="box blue"></div>
-          <div className="box red"></div>
-          <div className="box green"></div>
-          <div className="box blue"></div>
-        </div>
+      <section className="bestSellers">
+       <h2 className="bestSellers--title">Our Products</h2>
+       <div className="bestSellers--productsContainer">
+        <div className="big"></div>
+       </div>
       </section>
     </>
   );
